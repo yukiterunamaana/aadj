ede7b178123d0e0b64364f43f7101c13dc26190c
yukiterunamaana <kramarenko.miare13@yandex.ru>
2024-04-19 13:27:21 +0300

    added more detailed todos and more dependencies

---
+++ lib/key.dart
@@ -0,0 +1,42 @@
+String key = 'pPrTrg4wDuiQ5iP12QM8fVP016SHBhcH7fe8RQEodrQ';
+
+/*
+flutter: Status(
+id: 112296790718181415,
+url: https://mastodon.social/@aadj_test/112296790718181415,
+uri: https://mastodon.social/users/aadj_test/statuses/112296790718181415,
+content: <p>Toot!</p>,
+spoilerText: ,
+visibility: Visibility.public,
+favouritesCount: 0, repliesCount: 0, reblogsCount: 0,
+language: Language.english,
+inReplyToId: null,
+inReplyToAccountId: null,
+isFavourited: false, isReblogged: false, isMuted: false,
+isBookmarked: false, isSensitive: false, isPinned: false,
+lastStatusAt: null,
+
+account: Account(
+  id: 112282440600157454,
+  username: aadj_test, displayName: , acct: aadj_test, note: , url: https://mastodon.social/@aadj_test,
+  avatar: https://mastodon.social/avatars/original/missing.png,
+  avatarStatic: https://mastodon.social/avatars/original/missing.png,
+  header: https://mastodon.social/headers/original/missing.png,
+  headerStatic: https://mastodon.social/headers/original/missing.png,
+  followersCount: 0, followingCount: 1, subscribingCount: null, statusesCount: 2,
+  emojis: [],
+  fields: [Field(name: beep boop?, value: boop beep!, verifiedAt: null),
+           Field(name: From Russia, value: with love, verifiedAt: null),
+           Field(name: G_d let me finish, value: this is my thesis, verifiedAt: null)],
+  isDiscoverable: null,
+  isLocked: false,
+  isBot: true,
+  lastStatusAt: 2024-04-19 00:00:00.000, createdAt: 2024-04-16 00:00:00.000Z),
+
+  application: Application(name: Aadj, vapidKey: null, website: null),
+  poll: null, reblog: null,
+  mediaAttachments: [],
+  emojis: [], tags: [],
+  createdAt: 2024-04-19 07:50:41.967Z
+)
+ */
